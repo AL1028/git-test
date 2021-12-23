6fc2e4ab80406a359810d650881768c83660c5c9
Lee <Lee@qq.com>
2021-12-23 21:49:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author HUA
+ * @create 2021-12-23 21:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
